7d974ee89e828182e882f60ae0127ff000aa8abd
mahyarErshad <new@email>
2022-09-19 11:11:54 +0430

    Wrapper is done

--- src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
+++ src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 function TicketSearchSection() {
   return (
     <>
-    <Box sx={{height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px 35px 0 0",zIndex: "0"}}>
+    <Box sx={{height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px",zIndex: "0"}}>
       <Box sx={{width: "1260px",backgroundColor: "#fff", borderRadius:"30px",padding: "25px"}}>
-        <Box sx={{width:"100%",borderRadius: "15px",padding: "20px",background: "#f6f6f6"}}>
+        <Box sx={{width:"97%",borderRadius: "15px",padding: "20px",background: "#f6f6f6"}}>
 
         </Box>
       </Box>
