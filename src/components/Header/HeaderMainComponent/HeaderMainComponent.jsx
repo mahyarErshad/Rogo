e29c5aa7bb405c7ffec8f102e2ab6f74e622e02d
mahyarErshad <new@email>
2022-09-19 08:48:49 +0430

    Height and width set to 100%

--- src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
+++ src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function HeaderMainComponent() {
   return (
-    <Box sx={{position:"absolute",top:"0",right:"-15%", width:"130%",height:"450px",borderRadius:"0px 0 80% 80%", boxShadow:"inset 0px -14px 30px 0px rgba(0, 0, 0, 0.205)",background:"url(https://st.rogo.ir/assets/images/header-back/W-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundSize: "cover",overflow:"hidden"}}>
+    <Box sx={{position:"absolute",top:"0",right:"-15%", width:"130%",height:"450px",borderRadius:"0px 0 80% 80%", boxShadow:"inset 0px -14px 30px 0px rgba(0, 0, 0, 0.205)",background:"url(https://st.rogo.ir/assets/images/header-back/W-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundSize: "cover"}}>
         <div>hi</div>
     </Box>
   )
