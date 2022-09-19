121e69c9c5523aebeec657685e095d502bf893c2
mahyarErshad <new@email>
2022-09-19 09:50:23 +0430

    Justify space between

--- src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
+++ src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
@@ -6,7 +6,7 @@ import HeaderLogo from './HeaderLogo'
 function HeaderMainComponent() {
   return (
     <Box sx={{position:"absolute",top:"0",right:"-15%", width:"130%",height:"450px",borderRadius:"0px 0 80% 80%", boxShadow:"inset 0px -14px 30px 0px rgba(0, 0, 0, 0.205)",background:"url(https://st.rogo.ir/assets/images/header-back/W-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundSize: "cover"}}>
-          <Box sx={{display:"flex",alignItems:"center", justifyContent:"center",position:"relative",zIndex:"9",width:"1260px",height:"92px",mx:"auto",mt:"2rem"}}>
+          <Box sx={{display:"flex",alignItems:"center", justifyContent:"space-between",position:"relative",zIndex:"9",width:"1260px",height:"92px",mx:"auto",mt:"2rem"}}>
           <HeaderLogo />
           <HeaderLoginButton />
           </Box>
