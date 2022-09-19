9d290dc24a4708f06a3fe4fcb50c22b89dfe2c2d
mahyarErshad <new@email>
2022-09-19 12:46:24 +0430

    Imported TicketSearchingRadioButton

--- src/components/Body/TicketSearchingSection/MainTicketSearchingSection/MainTicketSearchSection.jsx
+++ src/components/Body/TicketSearchingSection/MainTicketSearchingSection/MainTicketSearchSection.jsx
@@ -1,17 +1,16 @@
 import { Box } from '@mui/system'
 import React from 'react'
 import TicketSearchingButtonGroup from '../TicketSearchingButtonGroup/TicketSearchingButtonGroup'
+import TicketSearchingRadioButton from '../TicketSearchingRadioButton/TicketSearchingRadioButton'
 
 function TicketSearchSection() {
   return (
     <>
     <Box sx={{height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px",zIndex: "0"}}>
       <Box sx={{width: "1260px",backgroundColor: "#fff", borderRadius:"30px",padding: "25px"}}>
-        <Box sx={{width:"97%",borderRadius: "15px",padding: "20px",background: "#f6f6f6",display:"flex",justifyContent:"flex-end"}}>
+        <Box sx={{width:"97%",borderRadius: "15px",padding: "20px",background: "#f6f6f6",display:"flex",justifyContent:"Space-between",flexDirection:"row-reverse"}}>
           <TicketSearchingButtonGroup />
-        </Box>
-        <Box>
-          
+        <TicketSearchingRadioButton />
         </Box>
       </Box>
     </Box>
