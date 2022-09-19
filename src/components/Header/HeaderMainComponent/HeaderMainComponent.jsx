79a4dbb3c0a763bcfdfd11311b3e9b547a162e1c
mahyarErshad <new@email>
2022-09-19 10:16:20 +0430

    Wrapped the button and the navlinks into a Box

--- src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
+++ src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
@@ -9,8 +9,10 @@ function HeaderMainComponent() {
     <Box sx={{position:"absolute",top:"0",right:"-15%", width:"130%",height:"450px",borderRadius:"0px 0 80% 80%", boxShadow:"inset 0px -14px 30px 0px rgba(0, 0, 0, 0.205)",background:"url(https://st.rogo.ir/assets/images/header-back/W-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundSize: "cover"}}>
           <Box sx={{display:"flex",alignItems:"center", justifyContent:"space-between",position:"relative",zIndex:"9",width:"1260px",height:"92px",mx:"auto",mt:"2rem"}}>
           <HeaderLogo />
+          <Box sx={{display:"flex",flexDirection:"row",gap:"1.2rem"}}>
           <HeaderNavigationLink text="صفحه اصلی" />
           <HeaderLoginButton />
+          </Box>
           </Box>
     </Box>
   )
