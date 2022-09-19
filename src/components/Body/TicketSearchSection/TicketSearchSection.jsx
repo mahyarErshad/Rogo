045060d75ce81420e7c104564786677a892d227a
mahyarErshad <new@email>
2022-09-19 10:33:04 +0430

    Created the wrapper

--- src/components/Body/TicketSearchSection/TicketSearchSection.jsx
+++ src/components/Body/TicketSearchSection/TicketSearchSection.jsx
@@ -1,8 +1,13 @@
+import { Box } from '@mui/system'
 import React from 'react'
 
 function TicketSearchSection() {
   return (
-    <div>TicketSearchSectionj</div>
+    <>
+    <Box sx={{width: "85%",height: "4.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px 35px 0 0",position: "absolute",zIndex: "0",left: "50%",transform: "translateX(-50%)",top: "-10px"}}>
+
+    </Box>
+    </>
   )
 }
 
