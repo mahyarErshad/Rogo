07d8658ad6fb1be218cbd334488b727a00e68085
mahyarErshad <new@email>
2022-09-19 10:14:26 +0430

    HeaderNavigationLink is working well

--- src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
+++ src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import HeaderLoginButton from '../HeaderLoginButton/HeaderLoginButton'
+import HeaderNavigationLink from '../HeaderNavigationLink/HeaderNavigationLink'
 import HeaderLogo from './HeaderLogo'
 
 function HeaderMainComponent() {
@@ -8,6 +9,7 @@ function HeaderMainComponent() {
     <Box sx={{position:"absolute",top:"0",right:"-15%", width:"130%",height:"450px",borderRadius:"0px 0 80% 80%", boxShadow:"inset 0px -14px 30px 0px rgba(0, 0, 0, 0.205)",background:"url(https://st.rogo.ir/assets/images/header-back/W-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundSize: "cover"}}>
           <Box sx={{display:"flex",alignItems:"center", justifyContent:"space-between",position:"relative",zIndex:"9",width:"1260px",height:"92px",mx:"auto",mt:"2rem"}}>
           <HeaderLogo />
+          <HeaderNavigationLink text="صفحه اصلی" />
           <HeaderLoginButton />
           </Box>
     </Box>
