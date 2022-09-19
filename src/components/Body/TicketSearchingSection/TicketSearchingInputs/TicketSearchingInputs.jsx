d1503c49e380b572537c6e804676b0c44be1b8d5
mahyarErshad <new@email>
2022-09-19 13:51:00 +0430

    Container is done

--- src/components/Body/TicketSearchingSection/TicketSearchingInputs/TicketSearchingInputs.jsx
+++ src/components/Body/TicketSearchingSection/TicketSearchingInputs/TicketSearchingInputs.jsx
@@ -1,9 +1,13 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
-function TicketSearchingInputs() {
+function TicketSearchingInputs(props) {
+    const {containerWidth} = props.width
   return (
     <>
-    
+    <Box sx={{width: containerWidth, border:"1px solid #e7e7e7",padding:"15px", borderRadius:"15px","&:hover":{borderColor: "rgba(0, 0, 0, 0.3)"} }}>
+
+    </Box>
     </>
   )
 }
