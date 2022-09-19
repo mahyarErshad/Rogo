17a6efa8d377b0fd0ed492a8ad2c422b83dc45fc
mahyarErshad <new@email>
2022-09-19 11:13:41 +0430

    TicketSearchingButton

--- src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
+++ src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
@@ -7,7 +7,7 @@ function TicketSearchSection() {
     <Box sx={{height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px",zIndex: "0"}}>
       <Box sx={{width: "1260px",backgroundColor: "#fff", borderRadius:"30px",padding: "25px"}}>
         <Box sx={{width:"97%",borderRadius: "15px",padding: "20px",background: "#f6f6f6"}}>
-
+          
         </Box>
       </Box>
     </Box>
---
+++ src/components/Body/TicketSearchingButton/TicketSearchingButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TicketSearchingButton() {
+  return (
+    <div>TicketSearchingButton</div>
+  )
+}
+
+export default TicketSearchingButton
