7aec651aaaca6ec74086b9deefe77c5e64016b42
mahyarErshad <new@email>
2022-09-19 09:06:50 +0430

    Completed Logo

--- src/components/Header/HeaderMainComponent/HeaderLogo.jsx
+++ src/components/Header/HeaderMainComponent/HeaderLogo.jsx
@@ -1,11 +1,15 @@
+import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 
 function HeaderLogo() {
   return (
-    <Box component="img" src="https://rogo.ir/assets/images/logo-persian.png" alt="logo" />
-
-
+    <>
+    <Box sx={{display:"flex", flexDirection:"column",alignItems:"center",justifyContent:"center",position:"relative",zIndex:"9",width:"1260px",height:"92px",mx:"auto",mt:"2rem"}}>
+    <Box sx={{width:"125px",height:"61px"}} component="img" src="https://rogo.ir/assets/images/logo-persian.png" alt="logo" />
+    <Typography component="h2" sx={{color: "#fff", fontSize: "1.22rem", paddingTop: "5px", textalign: "center"}}>خرید بلیط هواپیما</Typography>
+    </Box>
+    </>
   )
 }
 
