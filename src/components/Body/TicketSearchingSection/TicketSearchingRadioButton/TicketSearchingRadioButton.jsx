e78ba75a6dc32e0707d638269a465dd54450750c
mahyarErshad <new@email>
2022-09-19 12:51:50 +0430

    Cleaned a little bit

--- src/components/Body/TicketSearchingSection/TicketSearchingRadioButton/TicketSearchingRadioButton.jsx
+++ src/components/Body/TicketSearchingSection/TicketSearchingRadioButton/TicketSearchingRadioButton.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { pink } from '@mui/material/colors';
 import Radio from '@mui/material/Radio';
+import { Box } from '@mui/system';
 
 export default function TicketSearchingRadioButton() {
-  const [selectedValue, setSelectedValue] = React.useState('a');
+  const [selectedValue, setSelectedValue] = React.useState("oneWay");
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
@@ -18,20 +19,11 @@ export default function TicketSearchingRadioButton() {
   });
 
   return (
-    <div>
-      <Radio {...controlProps('a')} />
-      <Radio {...controlProps('b')} color="secondary" />
-      <Radio {...controlProps('c')} color="success" />
-      <Radio {...controlProps('d')} color="default" />
-      <Radio
-        {...controlProps('e')}
-        sx={{
-          color: pink[800],
-          '&.Mui-checked': {
-            color: pink[600],
-          },
-        }}
-      />
-    </div>
+    <>
+    <Box sx={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <Radio {...controlProps('oneWay')} />
+      <Radio {...controlProps('twoWays')} />
+    </Box>
+    </>
   );
 }
