acf405a6fdb59f46bb7fd5d1bcc53a57e7f215f8
mahyarErshad <new@email>
2022-09-19 09:24:12 +0430

    Imported HeaderLoginButton

--- src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
+++ src/components/Header/HeaderMainComponent/HeaderMainComponent.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import HeaderLoginButton from '../HeaderLoginButton/HeaderLoginButton'
 import HeaderLogo from './HeaderLogo'
 
 function HeaderMainComponent() {
   return (
     <Box sx={{position:"absolute",top:"0",right:"-15%", width:"130%",height:"450px",borderRadius:"0px 0 80% 80%", boxShadow:"inset 0px -14px 30px 0px rgba(0, 0, 0, 0.205)",background:"url(https://st.rogo.ir/assets/images/header-back/W-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundSize: "cover"}}>
           <HeaderLogo />
+          <HeaderLoginButton />
     </Box>
   )
 }
