c0efd4b4026e23fb04b98f906e6d825d0703f0dc
mahyarErshad <new@email>
2022-09-19 10:52:48 +0430

    Setting up width and height

--- src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
+++ src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 function TicketSearchSection() {
   return (
     <>
-    <Box sx={{width: "55%",height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px 35px 0 0",zIndex: "0",transform: "translateX(-50%)",position: "absolute",top: "165px",left: "50%"}}>
-      <Box sx={{backgroundColor: "#fff", borderRadius:"30px",position: "relative",padding: "25px"}}>
+    <Box sx={{width: "50%",height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px 35px 0 0",zIndex: "0",transform: "translateX(-50%)",position: "absolute",top: "165px",left: "50%"}}>
+      <Box sx={{width: "1000px",backgroundColor: "#fff", borderRadius:"30px",position: "relative",right:"54px",padding: "25px",top:"10px"}}>
 
       </Box>
     </Box>
