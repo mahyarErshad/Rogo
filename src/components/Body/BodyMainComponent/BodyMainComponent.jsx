894053050118c33fe81824dab65e40e0e2fdb666
mahyarErshad <new@email>
2022-09-19 11:04:34 +0430

    Using flex instead of position

--- src/components/Body/BodyMainComponent/BodyMainComponent.jsx
+++ src/components/Body/BodyMainComponent/BodyMainComponent.jsx
@@ -1,10 +1,13 @@
+import { Box } from '@mui/system'
 import React from 'react'
 import TicketSearchSection from '../TicketSearchSectionj/TicketSearchSection'
 
 function BodyMainComponent() {
   return (
     <>
+    <Box sx={{display:"flex",width:"100%",flexDirection:"column", alignItems:"center", justifyContent:"center",mt:"11.5rem"}}>
     <TicketSearchSection />
+    </Box>
     </>
   )
 }
