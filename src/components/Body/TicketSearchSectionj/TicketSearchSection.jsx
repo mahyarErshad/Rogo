f90c52d468dec421f637313d586105481aff6f14
mahyarErshad <new@email>
2022-09-19 10:55:27 +0430

    Changing position to flex

--- src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
+++ src/components/Body/TicketSearchSectionj/TicketSearchSection.jsx
@@ -4,9 +4,11 @@ import React from 'react'
 function TicketSearchSection() {
   return (
     <>
-    <Box sx={{width: "50%",height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px 35px 0 0",zIndex: "0",transform: "translateX(-50%)",position: "absolute",top: "165px",left: "50%"}}>
-      <Box sx={{width: "1000px",backgroundColor: "#fff", borderRadius:"30px",position: "relative",right:"54px",padding: "25px",top:"10px"}}>
+    <Box sx={{width: "50%",height: "2.5%",backgroundColor: "rgba(0, 0, 0, 0.3)",borderRadius: "35px 35px 0 0",zIndex: "0",transform: "translateX(-50%)"}}>
+      <Box sx={{width: "1000px",backgroundColor: "#fff", borderRadius:"30px",padding: "25px"}}>
+        <Box sx={{width:"100%",borderRadius: "15px",padding: "20px",background: "#f6f6f6"}}>
 
+        </Box>
       </Box>
     </Box>
     </>
