a4fca09e1b583ed3a18d2b87d5e20e515c84e15a
mahyarErshad <new@email>
2022-09-19 10:33:42 +0430

    Imported BodyMainComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import BodyMainComponent from "./components/Body/BodyMainComponent/BodyMainComponent";
 import HeaderMainComponent from "./components/Header/HeaderMainComponent/HeaderMainComponent";
 
 function App() {
   return (
     <>
       <HeaderMainComponent />
+      <BodyMainComponent />
     </>
   );
 }
