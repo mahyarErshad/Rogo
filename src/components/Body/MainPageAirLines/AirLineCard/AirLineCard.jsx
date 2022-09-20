08e87c67b7c29a0c56f31ed78822fe5c4f68bd49
mahyarErshad <new@email>
2022-09-20 14:22:47 +0430

    Added 2 photos

--- src/components/Body/MainPageAirLines/AirLineCard/AirLineCard.jsx
+++ src/components/Body/MainPageAirLines/AirLineCard/AirLineCard.jsx
@@ -1,14 +1,16 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 function AirLineCard(props) {
     const {image} = props
+    const [isHover, setIsHover] = useState(false)
   return (
-    <>
+    <div>
     <Box sx={{height:"172px", width:"199px", bgcolor:"red", border: "1px solid transparent", transition: "all 0.3s ease-in-out 0.1s", borderRadius:"20px",display:"flex", justifyContent:"center", alignItems:"center"}}>
         <Box component="img" sx={{height:"70%", borderRadius:"20px", objectFit:"cover"}} src={image}></Box>
+
     </Box>
-    </>
+    </div>
   )
 }
 
