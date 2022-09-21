1a20c305f4cbbe66d8531ec601f211c7e485e3ba
mahyarErshad <new@email>
2022-09-21 11:25:50 +0430

    Created FooterInformations

---
+++ src/components/Footer/FooterInformations/FooterInformations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FooterInformations() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default FooterInformations
--- src/components/Footer/FooterMain/FooterMain.jsx
+++ src/components/Footer/FooterMain/FooterMain.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import FooterInformations from '../FooterInformations/FooterInformations'
 import FooterLinksAndButtons from '../FooterLinksAndButtons/FooterLinksAndButtons'
 
 function FooterMain() {
   return (
     <>
     <FooterLinksAndButtons />
+    <FooterInformations />
     </>
   )
 }
