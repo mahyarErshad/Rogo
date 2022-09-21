5e0e4330fd1a9b76811e7cf06e3307c40d7bfe10
mahyarErshad <new@email>
2022-09-21 13:12:40 +0430

    Using box shadows

--- src/components/Utils/FooterCertifications/FooterCertifications.jsx
+++ src/components/Utils/FooterCertifications/FooterCertifications.jsx
@@ -6,7 +6,7 @@ function FooterCertifications(props) {
     const width = props.width || "60px"
   return (
     <>
-        <Box sx={{width, height:"50px", backgroundColor:"#fff", p:"0.3rem", borderRadius:"10px"}} component='img' src={image} alt="certificates" />
+      <Box sx={{width, cursor:"pointer", height:"50px", backgroundColor:"#fff", p:"0.3rem", borderRadius:"10px", "&:hover":{boxShadow: "-5px 8px 17px 0 rgba(0, 0, 0, 0.15)"}}} component='img' src={image} alt="certificates" />
     </>
   )
 }
