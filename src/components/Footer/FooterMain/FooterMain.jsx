0fcc77d1c56ea5c4cd84625337542961823da114
mahyarErshad <new@email>
2022-09-21 09:16:48 +0430

    width: "100%"

--- src/components/Footer/FooterMain/FooterMain.jsx
+++ src/components/Footer/FooterMain/FooterMain.jsx
@@ -5,7 +5,7 @@ import FooterNavigationLink from '../FooterNavigationLink/FooterNavigationLink'
 function FooterMain() {
   return (
     <>
-    <Box component="footer" sx={{backgroundColor:"#f9f9f9", borderRadius:"10px", width: "1260px", display:"flex", alignItems:"center", justifyContent:"center", mx:"auto", transform: "translateY(-60px)"}}>
+    <Box component="footer" sx={{backgroundColor:"#f9f9f9", borderRadius:"10px", width: "100%", display:"flex", alignItems:"center", justifyContent:"center", mx:"auto", transform: "translateY(-60px)"}}>
     <FooterNavigationLink />
     </Box>
     </>
