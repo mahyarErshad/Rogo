8bb12a43bf7eccf670da563c1ac3a96eafa6ffff
mahyarErshad <new@email>
2022-09-21 09:16:07 +0430

    Animating footerNavLink is done

--- src/components/Footer/FooterNavigationLink/FooterNavigationLink.jsx
+++ src/components/Footer/FooterNavigationLink/FooterNavigationLink.jsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/system'
 import React from 'react'
+import "../../../styles/footerNavLink/footerNavlink.css"
 
 function FooterNavigationLink() {
   return (
@@ -9,7 +10,7 @@ function FooterNavigationLink() {
             <Box sx={{fontSize:"1rem", mt:"1.5rem", fontWeight:"bold", mb:"0.9rem"}} dir="rtl" variant="h5">
                 دسترسی سریع ...
             </Box>
-            <Box dir="rtl" sx={{fontSize:"0.9rem", fontWeight:"100", mt:"0.7rem"}}>
+            <Box dir="rtl" className="footerNavLink" sx={{fontSize:"0.9rem", fontWeight:"100", mt:"0.7rem", transition: "all 0.3s ease-in-out 0.01s", color:"#a6a6a6", "&:hover":{color:"#e60f44", paddingRight:"15px"}}}>
                 صفحه اصلی
             </Box>
         </Box>
