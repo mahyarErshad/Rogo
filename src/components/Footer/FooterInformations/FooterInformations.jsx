73fbbf12843016334feb68e8b4a24e2fcbb1184b
mahyarErshad <new@email>
2022-09-21 12:43:43 +0430

    MarketPlaceButton are completed

--- src/components/Footer/FooterInformations/FooterInformations.jsx
+++ src/components/Footer/FooterInformations/FooterInformations.jsx
@@ -2,6 +2,9 @@ import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React, { memo } from 'react'
 import bazar from "../../../assets/images/footerImages/bazar.png"
+import myket from "../../../assets/images/footerImages/myket.png"
+import bazarHovered from "../../../assets/images/footerImages/bazarHovered.png"
+import myketHovered from "../../../assets/images/footerImages/myketHovered.png"
 import MarketPlaceButton from '../../Utils/MarketPlaceButton/MarketPlaceButton'
 
 function FooterInformations() {
@@ -17,7 +20,7 @@ function FooterInformations() {
             روگو، سامانه آنلاین خرید بلیط هواپیما از هر جای دنیا به هر جای دنیاست که به در آن واحد به شما امکان بررسی و خرید پرواز های همه ایرلاین ها دنیا را می دهد. تمامی بلیط های سیستمی و چارتری خریداری شده در روگو شامل بیمه لغو پرواز و پشتیبانی تا لحظه سوار شدن به هواپیما هستند. 
             </Typography>
         </Box>
-        <Box sx={{...wrapperStyle , gap:"0.7rem", backgroundColor:"#F6F6F6", mt:"0.8rem"}}>
+        <Box sx={{...wrapperStyle , gap:"0.7rem", backgroundColor:"#F6F6F6", mt:"0.8rem", borderRadius:"10px"}}>
             <Box sx={{background: "#e60f44", borderRadius: "17px", padding: "15px 20px", display:"flex", height:"70px", flexDirection:"column", width:"600px", justifyContent:"center", alignItems:"flex-end"}}>
                 <Typography dir="rtl" variant="p" sx={{color: "#fff", fontSize: "1em"}}>
                     راستی، یه هواپیما تو جیبت داری
@@ -26,7 +29,10 @@ function FooterInformations() {
                 اپلیکیشن روگو با پوشش سراسری بلیط چارتر و سیستمی و کاربری آسان و پشتیبانی 24 ساعته، فرایند خرید بلیط هواپیما را به تجربه ای مطمئن و لذت بخش تبدیل می کند. 
                 </Typography>
             </Box>
-            <MarketPlaceButton image={bazar} />
+            <Box sx={{display:"flex", alignItems:"center", justifyContent:"center", flexDirection:"row-reverse", alignSelf:"center", gap:"1rem"}}>
+            <MarketPlaceButton image={bazar} hoveredImage={bazarHovered} />
+            <MarketPlaceButton image={myket} hoveredImage={myketHovered} />
+            </Box>
         </Box>
     </Box>
     </>
--- src/components/Utils/MarketPlaceButton/MarketPlaceButton.jsx
+++ src/components/Utils/MarketPlaceButton/MarketPlaceButton.jsx
@@ -1,14 +1,16 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import { useState } from 'react'
 
 function MarketPlaceButton(props) {
     const {image , hoveredImage} = props
+    const [isHovered, setIsHovered] = useState(false)
   return (
-    <>
+    <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
     <Box>
-        <Box sx={{transition: "all 0.3s ease-in-out 0.01s", boxShadow: "-7px 8px 13px 0 rgba(0, 0, 0, 0.03)", cursor: "pointer", borderRadius: "13px"}} component="img" src={image} alt="ax" />
+        <Box sx={{transition: "all 0.3s ease-in-out 0.01s", boxShadow: "-7px 8px 13px 0 rgba(0, 0, 0, 0.03)", cursor: "pointer", borderRadius: "13px"}} component="img" src={isHovered ? hoveredImage : image} alt="ax" />
     </Box>
-    </>
+    </div>
   )
 }
 
